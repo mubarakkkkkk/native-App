66de5792a30fcd2aee832f72f213524ddede428c
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-11-06 20:33:13 +0100

    secound commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,19 +16,19 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        {/* <ThemedText type="title">Welcome!</ThemedText> */}
+        {/* <HelloWave /> */}
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
+        {/* <ThemedText type="subtitle">Step 1: Try it</ThemedText> */}
+        {/* <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
           </ThemedText>{' '}
           to open developer tools.
-        </ThemedText>
+        </ThemedText> */}
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
